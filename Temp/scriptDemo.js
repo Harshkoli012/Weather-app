117c6b8a3e5b564d37551ed2dd50563805f1a133
Harshad Koli <koliharsh57@gmail.com>
2026-06-28 21:41:53 +0530

    Delete Temp directory

--- Temp/scriptDemo.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("addNewItem").addEventListener("click", function() {
-    let newItem = document.createElement("li");
-    newItem.textContent = "Toast";
-     document.getElementById("shoppingList").appendChild(newItem);
-});
